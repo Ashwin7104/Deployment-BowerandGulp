388622a43ad9d7cb08a5347bb5c7b8347042ce1e
ashwin7104 <ashwinkumar50@gmail.com>
2017-01-01 15:42:37 +0530

    Deployment Code

---
+++ DeploymentStage/Features/Feature1/Feature1.feature
@@ -0,0 +1,10 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="5e9614aa-47e0-4e5e-9021-7c7aa4050c15" description="SharePoint App Feature" featureId="5e9614aa-47e0-4e5e-9021-7c7aa4050c15" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="DeploymentStage Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="24ac2f5f-c58a-4066-8277-01e61e14865d" />
+    <projectItemReference itemId="6a28be39-0049-414c-9468-47cae29d15ef" />
+    <projectItemReference itemId="2cd203aa-822d-4eac-b85a-67bd4df75c31" />
+    <projectItemReference itemId="a5ec7ea7-d6f6-44c5-ba65-37bdae894c30" />
+    <projectItemReference itemId="09948702-8400-4b15-bc4d-057e14da672e" />
+  </projectItems>
+</feature>
---
+++ DeploymentStage/gulpfile.js
@@ -0,0 +1,60 @@
+﻿/// <binding BeforeBuild='default' />
+/**
+ * Created by Javed on 8/15/2015.
+ */
+'use strict';
+
+var gulp = require('gulp'),
+    gprint = require('gulp-print'),
+    runSequence = require('run-sequence'),
+    bundle = require('gulp-concat'),
+    minify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+var paths = {
+    "JSDest": 'App/',
+    "JSCompiled": 'Scripts/',
+    "BowerSrc": "bower_components/"
+}
+
+var config = {
+    "BowerFiles": [
+        paths.BowerSrc + 'jquery/dist/jquery.min.js',
+        paths.BowerSrc + 'angular/angular.min.js'
+    ],
+    "BundleSrc": [
+        paths.JSCompiled + '*.js',
+    ],
+    "BundleFileName": "ed.bundle.js",
+    "MinifyFileName": "ed.bundle.min.js"
+}
+
+gulp.task('compile', function () {
+    var tsc = gulp
+        .src(config.TypeScriptFiles)
+        .pipe(gprint())
+        .pipe(ts({ "noImplicitAny": true, "removeComments": true }));
+
+    return tsc.js.pipe(gulp.dest(paths.JSCompiled));
+});
+
+gulp.task('bower', function () {
+    return gulp
+        .src(config.BowerFiles)
+        .pipe(gprint())
+        .pipe(gulp.dest(paths.JSDest));
+});
+
+gulp.task('prod_build', function () {
+    return gulp
+        .src(config.BundleSrc)
+        .pipe(bundle(config.BundleFileName))
+        .pipe(gprint())
+      //  .pipe(rename(config.MinifyFileName))
+       // .pipe(minify())
+        .pipe(gulp.dest(paths.JSDest));
+});
+
+gulp.task('default', function () {
+    runSequence('bower', 'prod_build');
+});
